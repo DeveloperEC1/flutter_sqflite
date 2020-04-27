840627e55b093a1f3448ce4dbb64e80aec6cdc55
EliorCohen <elior123456123123@gmail.com>
2020-04-27 11:06:43 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SQFlite Demo',
+      title: 'SQFLite Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
