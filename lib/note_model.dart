78a7d09f3789cd681d8e7e2a561294a24b957fce
EliorCohen <elior123456123123@gmail.com>
2020-05-07 03:03:59 +0300

    update

--- lib/note_model.dart
+++ lib/note_model.dart
@@ -1,29 +1,23 @@
 class Note {
-  int _id;
-  String _name;
-  int _age;
+  int id;
+  String name;
+  int age;
 
-  Note(this._name, this._age);
-
-  int get id => _id;
-
-  String get name => _name;
-
-  int get age => _age;
+  Note(this.name, this.age);
 
   Map<String, dynamic> toMap() {
     var map = new Map<String, dynamic>();
-    if (_id != null) {
-      map['id'] = _id;
+    if (id != null) {
+      map['id'] = id;
     }
-    map['name'] = _name;
-    map['age'] = _age;
+    map['name'] = name;
+    map['age'] = age;
     return map;
   }
 
   Note.fromMap(Map<String, dynamic> map) {
-    this._id = map['id'];
-    this._name = map['name'];
-    this._age = map['age'];
+    this.id = map['id'];
+    this.name = map['name'];
+    this.age = map['age'];
   }
 }
